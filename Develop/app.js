44551029b094ac837b6d513e8fd7eb1c628a9e0f
Katherine Jones <kejone1414@gmail.com>
2020-05-07 21:29:15 -0400

    feat: write to file working

--- Develop/app.js
+++ Develop/app.js
@@ -15,7 +15,7 @@ const questions = [
         type: "list",
         name: "role",
         message: "What is the role of this employee?",
-        choices: ["Manager", "Engineer", "Intern"],
+        choices: ["Manager", "Engineer", "Intern"]
     },
     {
         type: "input",
@@ -85,57 +85,57 @@ const questions = [
     }
 ]
 
+const team = [];
+
 function init() {
     inquirer.prompt(questions)
     .then(answers => {
-        const managers = [];
-        const engineers = [];
-        const interns = [];
 
         switch (answers.role) {
             case "Manager":
                 let newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-                managers.push(newManager);
+                team.push(newManager);
                 break;
 
             case "Engineer":
                 let newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
-                engineers.push(newEngineer);
+                team.push(newEngineer);
                 break;
 
             case "Intern":
                 let newIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
-                interns.push(newIntern)
+                team.push(newIntern)
                 break;
         };
 
         if (answers.askAgain) {
             init();
+        } else {
+            const html = render(team);
+            writeToFile(html);
         };
-
-        return {managers, engineers, interns}
     })
     .catch(error => console.log(error));
 };
 
+function writeToFile(html) {
+    if (fs.existsSync(OUTPUT_DIR)) {
+        fs.writeFile(outputPath, html, function (error) {
+            if (error) {
+                throw error;
+            }
+            console.log("Successfully generated team.html.")
+        });
+    } else {
+        fs.mkdirSync(OUTPUT_DIR);
+
+        fs.writeFile(outputPath, html, function (error) {
+            if (error) {
+                throw error;
+            }
+            console.log("Successfully generated team.html.")
+        });
+    };
+};
+
 init();
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
